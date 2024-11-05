344f7b1aadfe1b4a60527cbee613ccaa3a5d5b1b
Maulana-king <agusrohim56@gmail.com>
2024-11-05 21:40:27 +0700

    the collection of books display in table

--- script.js
+++ script.js
@@ -10,26 +10,42 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
+//Add book
+addBookToLibrary('Abdul', 'Fufa', 110, true);
+addBookToLibrary('Kurama', 'Chichan', 281, false);
+
 //function to creat instance of book
 function addBookToLibrary(title, author, pages, isRead) {
     return myLibrary.push(new Book(title, author, pages, isRead));
 }
-//Add book
-addBookToLibrary('Abdul', 'Fufa', 110, true);
-addBookToLibrary('Kurama', 'Chichan', 281, false);
+
  
 //Display book to User
 //function for iterate over array of myLibrary
+
 function showBookCollection() {
-    const list = document.querySelector('.book-list');
+    const tableBody = document.querySelector('table tbody');
     //iterate over the array collection of books
     myLibrary.forEach(function(book) {
-        //create h1 element 
-        const h1 = document.createElement('h1');
-        //assign the content within h1
-        h1.textContent = book['title'];
-        //add h1 to the parent element
-        list.appendChild(h1);
+        //create all element for the table
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+
+        //gave an content text to the element 
+        th.textContent = book.title;
+        td1.textContent = book.author;
+        td2.textContent = book.pages;
+        td3.textContent = book.isRead;
+
+        //insert all the element above to the table body
+        tableBody.appendChild(tr);
+        tr.appendChild(th);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
     });
 }
 
