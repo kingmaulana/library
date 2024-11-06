d756c5471c6e9c465574830c3c007795c28d17e2
Maulana-king <agusrohim56@gmail.com>
2024-11-06 11:40:02 +0700

    make function delete an item within array when user click the delete button

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function Book(title, author, pages, isRead) {
 }
 
 //Add book
-// addBookToLibrary('A Tale of Two Cities', 'Charles Dickens', 110, true);
-// addBookToLibrary('Le Petit Prince', 'Antoine de Saint-Exupéry', 211, false);
-// addBookToLibrary('The Alchemist (O Alquimista)', 'Paulo Coelho', 290, false);
-// addBookToLibrary("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 563, false);
+addBookToLibrary('A Tale of Two Cities', 'Charles Dickens', 110, true);
+addBookToLibrary('Le Petit Prince', 'Antoine de Saint-Exupéry', 211, false);
+addBookToLibrary('The Alchemist (O Alquimista)', 'Paulo Coelho', 290, false);
+addBookToLibrary("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 563, false);
 // addBookToLibrary('And Then There Were None', 'Agatha Christie', 456, false);
 // addBookToLibrary('The Hobbit', 'Lewis Carroll', 647, false);
 // addBookToLibrary('The Da Vinci Code', '	Dan Brown', 266, false);
@@ -32,14 +32,22 @@ function addBookToLibrary(title, author, pages, isRead) {
 
 function showBookCollection() {
     const tableBody = document.querySelector('table tbody');
-    //create all element for the table
-    const tr = document.createElement('tr');
-    const th = document.createElement('th');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td3 = document.createElement('td');
+
     //iterate over the array collection of books
     myLibrary.forEach(function(book) {
+        //DONT FORGET TO CHANGE AGAIN!!!
+        //create all element for the table
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        const td3 = document.createElement('td');
+        // for button remove
+        const td4 = document.createElement('td');
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Delete'
+        //!!!!  
+
         //gave an content text to the element 
         th.textContent = book.title;
         td1.textContent = book.author;
@@ -52,10 +60,16 @@ function showBookCollection() {
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
+
+        // button remove
+        //create attribute with the same name with title book
+        removeBtn.setAttribute('reference-book', book.title);
+        removeBtn.className = 'remove'
+        td4.appendChild(removeBtn);
+        tr.appendChild(td4);
     });
 }
-
-//showBookCollection();
+showBookCollection();
 
 // Form and Button Handler
 const form = document.querySelector('form');
@@ -77,3 +91,23 @@ btn.addEventListener('click', (x) => {
     x.preventDefault();
     form.reset();
 });
+
+
+//Function button remove
+function buttonRemove() {
+    const btnRemove = document.querySelectorAll(".remove");
+    btnRemove.forEach(button => {
+        button.addEventListener('click', () => {
+            //console.log(myLibrary);
+            myLibrary.forEach((book) => {
+                if(book.title === button.attributes[0].value) {
+                    const index = myLibrary.indexOf(book);
+                    myLibrary.splice(index, 1);
+                } 
+            });
+            console.log(myLibrary);
+        })
+    });
+}
+console.log(myLibrary);
+buttonRemove();
