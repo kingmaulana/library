88e2b85c1d4c82ca5a21061618eb841b6288e0e4
Maulana-king <agusrohim56@gmail.com>
2024-11-06 22:11:31 +0700

    update the script, remove all table and form replacece with the new one

--- script.js
+++ script.js
@@ -29,16 +29,22 @@ function addBookToLibrary(title, author, pages, isRead) {
  
 //Display book to User
 //function for iterate over array of myLibrary
-
 function showBookCollection() {
-    const tableBody = document.querySelector('table tbody');
+    const container = document.querySelector('.container');
 
-    //create all element for the table
-    const tr = document.createElement('tr');
-    const th = document.createElement('th');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td3 = document.createElement('td');
+    //create all element for the book
+    const divMain = document.createElement('div');
+    const img = document.createElement('img');
+    const divDesc = document.createElement('div');
+    const pTitle = document.createElement('p');
+    const pAuthor = document.createElement('p');
+    const pPages = document.createElement('p');
+    const pRead = document.createElement('p');
+    const sTitle = document.createElement('strong');
+    const sAuthor = document.createElement('strong');
+    const sPages = document.createElement('strong');
+    const sRead = document.createElement('strong');
+    
     // for button remove
     const td4 = document.createElement('td');
     const removeBtn = document.createElement('button');
@@ -49,33 +55,41 @@ function showBookCollection() {
 
     //iterate over the array collection of books
     myLibrary.forEach(function(book) {
-        //gave an content text to the element 
-        tr.id = book.title;
-        th.textContent = book.title;
-        td1.textContent = book.author;
-        td2.textContent = book.pages;
-        //td3.textContent = book.isRead;
+        //give all the element above attribute
+        divMain.className = "card-book";
+        img.src = "img/book.png";
+        img.alt = "book";
+        divDesc.className = "desc-book";
 
-        //insert all the element above to the table body
-        tableBody.appendChild(tr);
-        tr.appendChild(th);
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
+        pTitle.className = "p-title";
+        pAuthor.className = "p-author";
+        pPages.className = "p-pages";
+        pRead.className = "p-reaad";
 
-        // button remove
-        //create attribute with the same name with title book
-        removeBtn.setAttribute('reference-book', book.title);
-        removeBtn.className = 'remove'
-        td4.appendChild(removeBtn);
-        tr.appendChild(td4);
+        sTitle.textContent = "Title : ";
+        sAuthor.textContent = "Author : ";
+        sPages.textContent = "Pages : ";
+        sRead.textContent = "Read : ";
 
-        // toggle - switch
-        input.className = 'checkbox';
-        input.id = book.title;
-        input.type = 'checkbox';
-        input.checked = book.isRead;
-        td3.appendChild(input);
+        //each element show related value of array object
+        pTitle.textContent = book.title;
+        pAuthor.textContent = book.author;
+        pPages.textContent = book.pages;
+        pRead.textContent = book.isRead;
+
+        //append all the skeleton element to the parent element
+        container.appendChild(divMain);
+        divMain.appendChild(img);
+        divMain.appendChild(divDesc);
+        divDesc.appendChild(pTitle);
+        divDesc.appendChild(pAuthor);
+        divDesc.appendChild(pPages);
+        divDesc.appendChild(pRead);
+
+        pTitle.prepend(sTitle);
+        pAuthor.prepend(sAuthor);
+        pPages.prepend(sPages);
+        pRead.prepend(sRead);
     });
 }
 showBookCollection();
@@ -102,53 +116,53 @@ btn.addEventListener('click', (x) => {
 
     //trigger here, because the script read the delete button 
     //at first so the new added button doesn't update automaticly
-    buttonRemove();
-    checkboxFunc();
+    // buttonRemove();
+    // checkboxFunc();
 });
-buttonRemove();
+// buttonRemove();
 
 //Function button remove
-function buttonRemove() {
-    //select the tr for manipulate later
-    const trTable = document.querySelectorAll("tr");
-    const btnRemove = document.querySelectorAll(".remove");
-    btnRemove.forEach(button => {
-        //add a listerner for each button
-        button.addEventListener('click', () => {
-            //iterate each array which storage all the books
-            myLibrary.forEach((book) => {
-                //check if the array object title is same with button id
-                if(book.title === button.attributes[0].value) {
-                    //make an index for reference when splice(delete)
-                    const index = myLibrary.indexOf(book);
-                    myLibrary.splice(index, 1);
-                    //for html element delete the item matching with array delete
-                    trTable.forEach((row) => {
-                        if(row.id === book.title){
-                            //delete the tr or row in table
-                            row.remove();
-                        }
-                    });
-                };
-            });
-        });
-    });
-};
+// function buttonRemove() {
+//     //select the tr for manipulate later
+//     const trTable = document.querySelectorAll("tr");
+//     const btnRemove = document.querySelectorAll(".remove");
+//     btnRemove.forEach(button => {
+//         //add a listerner for each button
+//         button.addEventListener('click', () => {
+//             //iterate each array which storage all the books
+//             myLibrary.forEach((book) => {
+//                 //check if the array object title is same with button id
+//                 if(book.title === button.attributes[0].value) {
+//                     //make an index for reference when splice(delete)
+//                     const index = myLibrary.indexOf(book);
+//                     myLibrary.splice(index, 1);
+//                     //for html element delete the item matching with array delete
+//                     trTable.forEach((row) => {
+//                         if(row.id === book.title){
+//                             //delete the tr or row in table
+//                             row.remove();
+//                         }
+//                     });
+//                 };
+//             });
+//         });
+//     });
+// };
 
 //console.log(myLibrary);
 //console.log(cbxButton);
-function checkboxFunc() {
-    const cbxButton = document.querySelectorAll("table input");
-    cbxButton.forEach((button) => {
-        button.addEventListener('click', () => {
-            myLibrary.forEach((item) => {
-                if(item.title === button.id && button.checked === true) {
-                    item.isRead = true;
-                } else {
-                    item.isRead = false;
-                }
-            });
-        });
-    });
-}
-checkboxFunc();
+// function checkboxFunc() {
+//     const cbxButton = document.querySelectorAll("table input");
+//     cbxButton.forEach((button) => {
+//         button.addEventListener('click', () => {
+//             myLibrary.forEach((item) => {
+//                 if(item.title === button.id && button.checked === true) {
+//                     item.isRead = true;
+//                 } else {
+//                     item.isRead = false;
+//                 }
+//             });
+//         });
+//     });
+// }
+// checkboxFunc();
