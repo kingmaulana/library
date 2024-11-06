972764201f5ec54a52d05c586795a76031500a8f
Maulana-king <agusrohim56@gmail.com>
2024-11-06 09:49:00 +0700

    add a form for input and logic when user input and click the button submit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 //Store all object of books
 const myLibrary = [];
 
@@ -11,8 +12,14 @@ function Book(title, author, pages, isRead) {
 }
 
 //Add book
-addBookToLibrary('Abdul', 'Fufa', 110, true);
-addBookToLibrary('Kurama', 'Chichan', 281, false);
+// addBookToLibrary('A Tale of Two Cities', 'Charles Dickens', 110, true);
+// addBookToLibrary('Le Petit Prince', 'Antoine de Saint-Exupéry', 211, false);
+// addBookToLibrary('The Alchemist (O Alquimista)', 'Paulo Coelho', 290, false);
+// addBookToLibrary("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 563, false);
+// addBookToLibrary('And Then There Were None', 'Agatha Christie', 456, false);
+// addBookToLibrary('The Hobbit', 'Lewis Carroll', 647, false);
+// addBookToLibrary('The Da Vinci Code', '	Dan Brown', 266, false);
+// addBookToLibrary('The Catcher in the Rye', 'J. D. Salinger', 785, false);
 
 //function to creat instance of book
 function addBookToLibrary(title, author, pages, isRead) {
@@ -25,15 +32,14 @@ function addBookToLibrary(title, author, pages, isRead) {
 
 function showBookCollection() {
     const tableBody = document.querySelector('table tbody');
+    //create all element for the table
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
     //iterate over the array collection of books
     myLibrary.forEach(function(book) {
-        //create all element for the table
-        const tr = document.createElement('tr');
-        const th = document.createElement('th');
-        const td1 = document.createElement('td');
-        const td2 = document.createElement('td');
-        const td3 = document.createElement('td');
-
         //gave an content text to the element 
         th.textContent = book.title;
         td1.textContent = book.author;
@@ -49,4 +55,25 @@ function showBookCollection() {
     });
 }
 
-showBookCollection();
+//showBookCollection();
+
+// Form and Button Handler
+const form = document.querySelector('form');
+const btn = document.querySelector('#add-btn');
+btn.addEventListener('click', (x) => {
+    //get all the input id
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const read = document.querySelector('#read');
+    //check the input before assign the value
+    if(title.checkValidity() && author.checkValidity() && pages.checkValidity() && read.checkValidity()) {
+        addBookToLibrary(title.value, author.value, pages.value, read.value);
+        //only able to create element when the input pass the validity
+        showBookCollection();
+    }
+    console.log(myLibrary);
+    //prevent the button to refresh when clicked
+    x.preventDefault();
+    form.reset();
+});
