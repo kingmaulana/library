53303635808833cce585524b3b144b02620083a0
kingmaulana <akbarquipper68@gmail.com>
2024-12-04 21:04:36 +0700

    update coded with little bit oop style

--- script.js
+++ script.js
@@ -4,29 +4,27 @@
 const myLibrary = [];
 
 //Object of Book 
-function Book(title, author, pages, isRead) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.isRead = isRead;
-};
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.isRead = isRead
+    }
+
+    static addBookToLibrary(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead
+
+        return myLibrary.push(new Book(this.title, this.author, this.pages, this.isRead))
+    }
+}
 
 //Add book
-addBookToLibrary('A Tale of Two Cities', 'Charles Dickens', 110, true);
-// addBookToLibrary('Le Petit Prince', 'Antoine de Saint-Exupéry', 211, true);
-// addBookToLibrary('The Alchemist (O Alquimista)', 'Paulo Coelho', 290, false);
-// addBookToLibrary("Harry Potter and the Philosopher's Stone", 'J. K. Rowling', 563, false);
-// addBookToLibrary('And Then There Were None', 'Agatha Christie', 456, false);
-// addBookToLibrary('The Hobbit', 'Lewis Carroll', 647, true);
-// addBookToLibrary('The Da Vinci Code', '	Dan Brown', 266, true);
-// addBookToLibrary('The Catcher in the Rye', 'J. D. Salinger', 785, false);
+Book.addBookToLibrary('A Tale of Two Cities', 'Charles Dickens', 110, true);
 
-//function to creat instance of book
-function addBookToLibrary(title, author, pages, isRead) {
-    return myLibrary.push(new Book(title, author, pages, isRead));
-};
-
- 
 //Display book to User
 //function for iterate over array of myLibrary
 function showBookCollection() {
@@ -49,78 +47,36 @@ function showBookCollection() {
             }
         }
     }
-        
-    //create all element for the book
-    const divMain = document.createElement('div');
-    const img = document.createElement('img');
-    const divDesc = document.createElement('div');
-    const pTitle = document.createElement('p');
-    const pAuthor = document.createElement('p');
-    const pPages = document.createElement('p');
-    const sTitle = document.createElement('strong');
-    const sAuthor = document.createElement('strong');
-    const sPages = document.createElement('strong');
-    const cLabel = document.createElement('label');
-    const cInput = document.createElement('input');
-    const cSpan = document.createElement('span');
-
-    // for button remove
-    const divImg = document.createElement('div');
-    const delImg = document.createElement('img');       
     
     //iterate over the array collection of books
+    let content = document.createElement('div');
     myLibrary.forEach(function(book) {
-        //give all the element above attribute
-        divMain.className = "card-book";
-        divMain.setAttribute("book-ref", book.title);
-        img.src = `img/bcover${lastNumber}.png`;
-        img.alt = "book";
-        divDesc.className = "desc-book";
-
-        pTitle.className = "p-title";
-        pAuthor.className = "p-author";
-        pPages.className = "p-pages";
-
-        cLabel.className = "is-read";
-        cInput.type = "checkbox";
-        cSpan.className = "checkmark";
-
-        sTitle.textContent = "Title : ";
-        sAuthor.textContent = "Author : ";
-        sPages.textContent = "Pages : ";
-        cLabel.textContent = "Finished";
-
-        divImg.className = "my-img"; 
-        delImg.src = "alpha-x-circle.svg";
-        delImg.alt = "x";
-        delImg.className = "remove";
-        delImg.setAttribute("book-ref", book.title);
-
-        //each element show related value of array object
-        pTitle.textContent = book.title;
-        pAuthor.textContent = book.author;
-        pPages.textContent = book.pages;
-        cInput.checked = book.isRead;
-
-        //append all the skeleton element to the parent element
-        container.appendChild(divMain);
-        divMain.appendChild(divImg);
-        divImg.appendChild(img);
-        divMain.appendChild(divDesc);
-        divDesc.appendChild(pTitle);
-        divDesc.appendChild(pAuthor);
-        divDesc.appendChild(pPages);
-
-        pTitle.prepend(sTitle);
-        pAuthor.prepend(sAuthor);
-        pPages.prepend(sPages);
-
-        divDesc.appendChild(cLabel);
-        cLabel.appendChild(cInput);
-        cLabel.appendChild(cSpan);
-        
-        divImg.appendChild(delImg);
-
+        content.className = "card-book"
+        content.setAttribute = ('book-ref', `${book.title}`)
+        content.innerHTML = 
+            `<div class="my-img">
+                <img src="img/bcover${lastNumber}.png" alt="book">
+                <img src="alpha-x-circle.svg" alt="x" class="remove" book-ref='${book.title}'>
+            </div>
+            <div class="desc-book">
+                <p class="p-title">
+                    <strong>Title: </strong>
+                    ${book.title}
+                </p>
+                <p class="p-author">
+                    <strong>Author: </strong>
+                    ${book.author}
+                </p>
+                <p class="p-pages">
+                    <strong>Pages: </strong>
+                    ${book.pages}
+                </p>
+                <label class="is-read">Finished
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+            </div>`
+        container.appendChild(content)
     });
 }
 showBookCollection();
@@ -136,7 +92,7 @@ btn.addEventListener('click', (x) => {
     const inputCheckbox = document.querySelector('input#switch');
     //check the input before assign the value
     if(title.checkValidity() && author.checkValidity() && pages.checkValidity()) {
-        addBookToLibrary(title.value, author.value, pages.value, inputCheckbox.checked);
+        Book.addBookToLibrary(title.value, author.value, pages.value, inputCheckbox.checked);
         //only able to create element when the input pass the validity
         showBookCollection();
     }
@@ -161,15 +117,16 @@ function buttonRemove() {
         //add a listerner for each button
         button.addEventListener('click', () => {
             //iterate each array which storage all the books
-            myLibrary.forEach((book) => {
+            myLibrary.forEach((book) => {                
                 //check if the array object title is same with button id
-                if(book.title === button.attributes[3].value) {
+                if(book.title === button.attributes[3].value) {                    
                     //make an index for reference when splice(delete)
                     const index = myLibrary.indexOf(book);
-                    myLibrary.splice(index, 1);
+                    myLibrary.splice(index, 1);                    
                     //for html element delete the item matching with array delete
                     boxBook.forEach((item) => {
-                        if(item.attributes[1].value === book.title){
+                        let bookItem = item.attributes[0].ownerElement.setAttribute;
+                        if(bookItem === book.title){
                             //delete the tr or row in table
                             item.remove();
                         }
